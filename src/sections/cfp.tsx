8b588116d839313dae2998819382e4a6a8604a6f
Mia <mia_bajic@live.com>
2023-11-14 00:24:56 +0100

    Formatting

--- src/sections/cfp.tsx
+++ src/sections/cfp.tsx
@@ -2,7 +2,7 @@ import { Container, Heading, Paragraph } from "theme-ui";
 
 export const CFP = () => (
     <Container id="cfp" variant="smallContainer">
-        <Heading>Call For Proposal by 31st December</Heading>
+        <Heading>Call For Proposals by 31st December</Heading>
         <Paragraph>
             <a
                 href="https://docs.google.com/forms/d/e/1FAIpQLSfaDox_HZ2il0KOoohtYQJxVbhuTlwMFpmbz8bp-WOoLVQlNQ/viewform?usp=sf_link"
@@ -11,23 +11,16 @@ export const CFP = () => (
             >
                 Share
             </a>{" "}
-            with us your ideas! Each talk will be 10-minute long. They
-            can be given in person or remote. We will have a live stream of the event.
-        </Paragraph>
-        <Paragraph>
-            The applications are reviewed on a rolling basis.{" "}
-            <a
-                href="https://docs.google.com/forms/d/e/1FAIpQLSfaDox_HZ2il0KOoohtYQJxVbhuTlwMFpmbz8bp-WOoLVQlNQ/viewform?usp=sf_link"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Apply now
-            </a>{" "}
-            to secure a spot and join the party 🎊.
-            <br />
+            with us your ideas! Each talk will be 10-minute long. They can be given in person only. We will have a live stream of the event.
+            The applications are reviewed on a rolling basis. Apply now to secure a spot and join the party 🎊.
+
+            <br></br>
+
             We are looking for first-time speakers! It is a good opportunity to get started with public speaking
             with these mini 10-min talks! 🎤
-            <br />
+
+            <br></br>
+
             We are looking for experienced speakers! It is a great opportunity to inspire a new generation of 🐍 loving people!
         </Paragraph>
     </Container>
