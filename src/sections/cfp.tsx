e450d8756c9f4e14a5499910b7aca0c2f17bf1b9
Mia <mia_bajic@live.com>
2023-11-14 00:20:57 +0100

    Add button to header

--- src/sections/cfp.tsx
+++ src/sections/cfp.tsx
@@ -1,32 +1,34 @@
 import { Container, Heading, Paragraph } from "theme-ui";
 
 export const CFP = () => (
-  <Container id="cfp" variant="smallContainer">
-    <Heading><s>Call For Proposal by 31st December</s></Heading>
-    <Paragraph>
-      <a
-        href="https://docs.google.com/forms/d/e/1FAIpQLSfaDox_HZ2il0KOoohtYQJxVbhuTlwMFpmbz8bp-WOoLVQlNQ/viewform?usp=sf_link"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Share
-      </a>{" "}
-      with us your 💥<b>ideas</b>💥️ ! Each talk will be 10-minute long. They
-      can be given in person or remote. We will have a live stream of the event.
-    </Paragraph>
-    <Paragraph>
-      The applications are reviewed on a rolling basis.{" "}
-      <a
-        href="https://docs.google.com/forms/d/e/1FAIpQLSfaDox_HZ2il0KOoohtYQJxVbhuTlwMFpmbz8bp-WOoLVQlNQ/viewform?usp=sf_link"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Apply now
-      </a>{" "}
-      to secure a spot and join the party 🎊. We are looking for first-time
-      speakers! 🎤 It is a good opportunity to get started with public speaking
-      with these mini 10-min talks! ✅ We are looking for experienced speakers!
-      It is a great opportunity to inspire a new generation of 🐍 loving people!
-    </Paragraph>
-  </Container>
+    <Container id="cfp" variant="smallContainer">
+        <Heading>Call For Proposal by 31st December</Heading>
+        <Paragraph>
+            <a
+                href="https://docs.google.com/forms/d/e/1FAIpQLSfaDox_HZ2il0KOoohtYQJxVbhuTlwMFpmbz8bp-WOoLVQlNQ/viewform?usp=sf_link"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Share
+            </a>{" "}
+            with us your ideas! Each talk will be 10-minute long. They
+            can be given in person or remote. We will have a live stream of the event.
+        </Paragraph>
+        <Paragraph>
+            The applications are reviewed on a rolling basis.{" "}
+            <a
+                href="https://docs.google.com/forms/d/e/1FAIpQLSfaDox_HZ2il0KOoohtYQJxVbhuTlwMFpmbz8bp-WOoLVQlNQ/viewform?usp=sf_link"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Apply now
+            </a>{" "}
+            to secure a spot and join the party 🎊.
+            <br />
+            We are looking for first-time speakers! It is a good opportunity to get started with public speaking
+            with these mini 10-min talks! 🎤
+            <br />
+            We are looking for experienced speakers! It is a great opportunity to inspire a new generation of 🐍 loving people!
+        </Paragraph>
+    </Container>
 );
