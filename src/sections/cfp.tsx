641431230c79662dd0c346a00ec5441055993599
Artur Czepiel <czepiel.artur@gmail.com>
2023-12-02 15:54:51 +0100

    tweak cfp section
    Mostly to check if the deployment is invalidating cache correctly.

--- src/sections/cfp.tsx
+++ src/sections/cfp.tsx
@@ -3,29 +3,27 @@ import { ContentButton } from "~/components/header";
 
 export const CFP = () => (
     <Container id="cfp" variant="smallContainer">
-        <Heading>Call For Proposals by 20th December</Heading>
+        <Heading>Call For Proposals until 20th December</Heading>
         <Paragraph>
             <a
-                href="https://docs.google.com/forms/d/e/1FAIpQLSfaDox_HZ2il0KOoohtYQJxVbhuTlwMFpmbz8bp-WOoLVQlNQ/viewform?usp=sf_link"
+                href="https://forms.gle/LC3khsroakA8bi8GA"
                 target="_blank"
                 rel="noopener noreferrer"
             >
                 Share
             </a>{" "}
-            with us your ideas! Each talk will be 10-minute long. They can be given in person only. We will have a live stream of the event.
+            with us your ideas! Each talk will be 10-minutes long. They can be given in person only. We will have a live stream of the event.
             The applications are reviewed on a rolling basis. Apply now to secure a spot and join the party 🎊.
 
         </Paragraph>
-        <Paragraph>
+        <Paragraph sx={{ p: "2rem" }}>
             If you're not sure which topic you would like to talk about – you can send multiple proposals :)
         </Paragraph>
-        <br></br>
-        <Paragraph>
+        <Paragraph sx={{ p: "2rem" }}>
             We are looking for first-time speakers! It is a good opportunity to get started with public speaking
             with these mini 10-min talks! 🎤
         </Paragraph>
-        <br></br>
-        <Paragraph>
+        <Paragraph sx={{ p: "2rem" }}>
             We are looking for experienced speakers! It is a great opportunity to inspire a new generation of 🐍 loving people!
         </Paragraph>
 
